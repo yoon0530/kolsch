d2bdd1270895cc1f121d507e248c57dafb5db806
YunCheol <yoon0530@naver.com>
2024-09-28 17:02:07 +0900

    이름 추가

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div>
       <div className="container" style={{width:"600px", height:"500px "}}>
 
-      <h3 className="py-5 my-5" style={{textAlign:"center ", color:"#6a5353 "}}>기업연계프로젝트</h3>
+      <h3 className="py-5 my-5" style={{textAlign:"center ", color:"#6a5353 "}}>이윤철</h3>
       </div>
   </div>
   )
